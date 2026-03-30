51237b8055e215d75843d3508d9de5e1a4aab43f
vardhini330 <vardhinis57@gmail.com>
2026-03-30 23:35:25 +0530

    Create timeout.js

---
+++ javascript/timeout.js
@@ -0,0 +1,16 @@
+/**
+ * @param {Function} fn
+ * @param {Array} args
+ * @param {number} t
+ * @return {Function}
+ */
+const cancellable = function(fn, args, t) {
+    // cancelFn function//
+    const cancelFn = function (){
+      clearTimeout(timer);
+  };
+  const timer = setTimeout(()=>{
+      fn(...args)
+  }, t);
+  return cancelFn ;
+};
