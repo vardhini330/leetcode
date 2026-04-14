2f64ff315b71aecde3a4a6cd2cb0324679425ec6
vardhini330 <vardhinis57@gmail.com>
2026-04-15 00:02:19 +0530

    Implement ArrayWrapper with valueOf and toString methods

---
+++ javascript/array wrapper.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ */
+var ArrayWrapper = function(nums) {
+    this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function() {
+    return this.nums.reduce((sum, num) => sum + num, 0);
+};
+
+/**
+ * @return {string}
+ */
+ArrayWrapper.prototype.toString = function() {
+    return `[${this.nums.join(",")}]`;
+};
