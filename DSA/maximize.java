567b04c2aa39f1dff8a5a1c94c2e92b2ba2550e7
vardhini330 <vardhinis57@gmail.com>
2026-07-22 00:08:03 +0530

    Create maximize.java

---
+++ DSA/maximize.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxActiveSectionsAfterTrade(String s) {
+        int oneCount = 0; 
+        int maxMergedzeros = 0; 
+        int currZeroCount = 0; 
+        int lastZeroCount = 0;
+        for(char c : s.toCharArray()){
+            if(c == '0') currZeroCount++; 
+            else {
+                if(currZeroCount != 0) lastZeroCount = currZeroCount;
+                currZeroCount = 0; 
+                oneCount++; 
+            }
+            maxMergedzeros = Math.max(maxMergedzeros, currZeroCount + lastZeroCount);
+        }
+        if(maxMergedzeros == currZeroCount || maxMergedzeros == lastZeroCount) return oneCount;
+        return oneCount + maxMergedzeros;
+    }
+}
