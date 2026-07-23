0e7d7a5286f401f69bccfdced1003698ab33d8f6
vardhini330 <vardhinis57@gmail.com>
2026-07-23 18:06:20 +0530

    Add Solution class for unique XOR triplets calculation

---
+++ DSA/noofunique.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if(n < 3) return n;
+        int nearest = (int)(Math.log(n)/Math.log(2));
+        return 1 << (nearest + 1);
+    }
+}
