78bd04cb0ac61aed11d605e3322ef4eb1b2cca19
vardhini330 <vardhinis57@gmail.com>
2026-09-10 23:06:46 +0530

    Create findkthno.java

---
+++ DSA/findkthno.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findKthNumber(int m, int n, int k) {
+        int low = 1;
+        int high = m * n;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int count = 0;
+            for (int i = 1; i <= m; i++) {
+                count += Math.min(mid / i, n);
+            }
+            if (count >= k) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+}
